900c984d28f4a18ef93661e8d0863c95066cd7ef
BroNaz <brodnikovnv@gmail.com>
2021-02-23 21:02:22 +0300

    add loger

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,15 +1,37 @@
 package apiserver
 
+import "github.com/sirupsen/logrus"
+
+// APIServer - управляющая сущность
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
 }
 
+// New - возвращает дефолтный APIServer
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
+// Start - точка входа сервиса и конфигурация его
 func (s *APIServer) Start() error {
+	if err := s.configLogger(); err != nil {
+		return err
+	}
+	s.logger.Info("Start API server")
+
+	return nil
+}
+
+func (s *APIServer) configLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
 	return nil
 }
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,11 +1,15 @@
 package apiserver
 
+// Config - конфигуратор сервиса, заполняется из toml файла
 type Config struct {
 	BindAddr string `toml:"bint_addr"`
+	LogLevel string `toml:"log_level"`
 }
 
+// NewConfig - возвращает новый дефолтный конфиг
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
+		LogLevel: "debug",
 	}
 }
