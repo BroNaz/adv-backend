a7de8e794af69498b449649b502e1ed05a367315
BroNaz <brodnikovnv@gmail.com>
2021-02-23 19:54:25 +0300

    add config toml

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/BroNaz/adv-backend/internal/app/apiserver"
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to toml config")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	if _, err := toml.DecodeFile(configPath, config); err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
