0462e570788372d7ba3521d0b45a477ac21a313e
BroNaz <brodnikovnv@gmail.com>
2021-02-24 20:09:42 +0300

    add validation User

--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -7,6 +7,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestUser_Validate(t *testing.T) {
+	testCases := []struct {
+		nameTask string
+		user     func() *model.User
+		isValid  bool
+	}{
+		{
+			nameTask: "valid",
+			user: func() *model.User {
+				return model.TestUser(t)
+			},
+			isValid: true,
+		},
+		{
+			nameTask: "emty email",
+			user: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = ""
+				return u
+			},
+			isValid: false,
+		},
+		{
+			nameTask: "emty pswword",
+			user: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+				return u
+			},
+			isValid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.nameTask, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.user().Validate())
+			} else {
+				assert.Error(t, tc.user().Validate())
+			}
+		})
+	}
+
+}
 func TestUser_BeforeCreate(t *testing.T) {
 	u := model.TestUser(t)
 	assert.NoError(t, u.BeforeCreate())
